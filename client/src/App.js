0acd9a3afac70ead6989d2951d13a0db9cd04692
yangchen <b06902044@ntu.edu.tw>
2020-12-01 23:37:56 +0800

    auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,61 @@
 import React, { Component, useState , useEffect } from 'react';
 import './App.css';
 import axios from 'axios'
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
+firebase.initializeApp({
+  apiKey: "AIzaSyBDwcbLqFnlmWZEsF9B1HqFgYeW_5a-ft0",
+  authDomain: "trees-96888.firebaseapp.com"
+})
 
-function App(){
-  const [response, setResponse] = useState({});
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {auth: false};
+  }
 
-  useEffect(() => {
-    axios.get('/api/v1/say-something').then((res) => {
-      setResponse(res.data);
-    });
-  });
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
 
-  return (
-    <div className="App">
-        <h1>Here is out new future!</h1>
-        <h1>{response.body}</h1>
-    </div>
-  );
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({auth: !!user});
+      console.log("user", user);
+    })
+  }
+  
+  render(){
+    return(
+      <div className="App">
+        {this.state.auth ? (
+          <span>
+            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            <img
+              alt="profile picture"
+              src={firebase.auth().currentUser.photoURL}
+            />
+          </span>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
+    )
+  }
+
 }
 
 export default App;
